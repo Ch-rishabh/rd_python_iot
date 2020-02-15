22ffd779bdbb75559002438ad333cce4944cf705
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-02-15 15:22:20 +0530

    Add files via upload

---
+++ Demo_Shell_Output.py
@@ -0,0 +1,37 @@
+Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = 10
+>>> b = 20
+>>> c = a + b
+>>> print(c)
+30
+>>> c
+30
+>>> type(a)
+<class 'int'>
+>>> id(a)
+4348083120
+>>> dir(int)
+['__abs__', '__add__', '__and__', '__bool__', '__ceil__', '__class__', '__delattr__', '__dir__', '__divmod__', '__doc__', '__eq__', '__float__', '__floor__', '__floordiv__', '__format__', '__ge__', '__getattribute__', '__getnewargs__', '__gt__', '__hash__', '__index__', '__init__', '__init_subclass__', '__int__', '__invert__', '__le__', '__lshift__', '__lt__', '__mod__', '__mul__', '__ne__', '__neg__', '__new__', '__or__', '__pos__', '__pow__', '__radd__', '__rand__', '__rdivmod__', '__reduce__', '__reduce_ex__', '__repr__', '__rfloordiv__', '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__round__', '__rpow__', '__rrshift__', '__rshift__', '__rsub__', '__rtruediv__', '__rxor__', '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__', '__truediv__', '__trunc__', '__xor__', 'as_integer_ratio', 'bit_length', 'conjugate', 'denominator', 'from_bytes', 'imag', 'numerator', 'real', 'to_bytes']
+>>> dir
+<built-in function dir>
+>>> dir(a)
+['__abs__', '__add__', '__and__', '__bool__', '__ceil__', '__class__', '__delattr__', '__dir__', '__divmod__', '__doc__', '__eq__', '__float__', '__floor__', '__floordiv__', '__format__', '__ge__', '__getattribute__', '__getnewargs__', '__gt__', '__hash__', '__index__', '__init__', '__init_subclass__', '__int__', '__invert__', '__le__', '__lshift__', '__lt__', '__mod__', '__mul__', '__ne__', '__neg__', '__new__', '__or__', '__pos__', '__pow__', '__radd__', '__rand__', '__rdivmod__', '__reduce__', '__reduce_ex__', '__repr__', '__rfloordiv__', '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__round__', '__rpow__', '__rrshift__', '__rshift__', '__rsub__', '__rtruediv__', '__rxor__', '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__', '__truediv__', '__trunc__', '__xor__', 'as_integer_ratio', 'bit_length', 'conjugate', 'denominator', 'from_bytes', 'imag', 'numerator', 'real', 'to_bytes']
+>>> print("Sum of a and b is c")
+Sum of a and b is c
+>>> print("Sum of ", a,  "and",  b, "is", c)
+Sum of  10 and 20 is 30
+>>> 
+=================== RESTART: /Users/anmolrajarora/Documents/rd_college_python_iot/Demo.py ==================
+A is 10
+>>> 
+=================== RESTART: /Users/anmolrajarora/Documents/rd_college_python_iot/Demo.py ==================
+A is 10
+Sum of 10 and 20 is 30
+>>> 
+=================== RESTART: /Users/anmolrajarora/Documents/rd_college_python_iot/Demo.py ==================
+A is 10
+Sum of 10 and 20 is 30
+Sum of 10 and 20 is 30
+>>> 
